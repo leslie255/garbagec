4623a3129cc66e31e593a446fb0504e62095466f
Leslie Kerman <kerbal255@outlook.com>
2024-04-09 07:09:49 +0100

    Make empty objlist not have heap allocation

--- src/main.c
+++ src/main.c
@@ -4,11 +4,12 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct gcptr GcPtr;
 
-typedef struct obj_list {
-  /// Must be non null
+typedef struct objlist {
+  /// Null for init.
   GcPtr *items;
   usize len;
   usize cap;
@@ -50,17 +51,26 @@ static inline void println_gcptr_addr(GcPtr p) {
 
 static inline ObjList new_with_capacity_objlist(usize cap);
 
-static inline ObjList new_objlist() { return new_with_capacity_objlist(OBJLIST_INIT_CAP); }
+static inline ObjList new_objlist() { return (ObjList){0}; }
 
-/// Panics if cap is zero.
 static inline ObjList new_with_capacity_objlist(usize cap) {
-  ASSERT(cap != 0);
+  if (cap == 0) {
+    return new_objlist();
+  }
   GcPtr *p = xalloc(GcPtr, cap);
   return (ObjList){.items = p, .len = 0, .cap = cap};
 }
 
 static inline void push_objlist(ObjList *self, GcPtr item) {
-  DEBUG_ASSERT(self->items != nullptr);
+  if (self->items == nullptr) {
+    DEBUG_ASSERT(self->cap == 0);
+    DEBUG_ASSERT(self->len == 0);
+    self->cap = OBJLIST_INIT_CAP;
+    self->len = 1;
+    self->items = xalloc(GcPtr, self->cap);
+    self->items[0] = item;
+    return;
+  }
   self->len += 1;
   if (self->cap < self->len) {
     self->cap *= 2;
@@ -69,17 +79,17 @@ static inline void push_objlist(ObjList *self, GcPtr item) {
   self->items[self->len - 1] = item;
 }
 
-static inline void debug_check_range_objlist(ObjList *self, usize i) {
-  DEBUG_ASSERT(self->items != nullptr);
-  DEBUG_ASSERT(i < self->len);
-}
+static inline void debug_check_range_objlist(ObjList *self, usize i) { DEBUG_ASSERT(i < self->len); }
 
 static inline GcPtr *get_item_objlist(ObjList *self, usize i) {
   debug_check_range_objlist(self, i);
   return &self->items[i];
 }
 
-static inline void free_objlist(ObjList self) { xfree(self.items); }
+static inline void free_objlist(ObjList self) {
+  if (self.items != nullptr)
+    xfree(self.items);
+}
 
 /// Not thread safe.
 typedef struct gcarena {
@@ -145,12 +155,11 @@ void gc_sweep(GcArena *self) {
 }
 
 /// `value` is the unique pointer to the value on heap.
-GcPtr gc_new_object(GcArena *self, void *restrict value, ObjList reflist, DestroyCallback free_callback) {
-  DEBUG_ASSERT(value != nullptr);
+GcPtr gc_new_object(GcArena *self, void *restrict value, ObjList reflist, DestroyCallback destroy_callback) {
   ObjMetadata metadata = (ObjMetadata){
       .reflist = reflist,
       .strong_count = 1,
-      .destroy_callback = free_callback,
+      .destroy_callback = destroy_callback,
   };
   GcPtr obj = (GcPtr){
       .obj = value,
@@ -165,59 +174,77 @@ void gc_mark_dead(GcPtr object) {
   object.metadata->strong_count -= 1;
 }
 
-#define GC_DEREF(TY, PTR) (*(TY *)(PTR.obj))
+#define GC_GET(TY, PTR) ((const TY *)((PTR).obj))
 
 typedef struct test_obj {
   GcPtr child_i32_0;
-  GcPtr child_i32_1;
+  GcPtr string;
 } TestObj;
 
 ObjList test_obj_reflist(TestObj *self) {
   DEBUG_ASSERT(self->child_i32_0.obj != nullptr);
-  DEBUG_ASSERT(self->child_i32_1.obj != nullptr);
+  DEBUG_ASSERT(self->string.obj != nullptr);
   ObjList reflist = new_with_capacity_objlist(2);
   push_objlist(&reflist, self->child_i32_0);
-  push_objlist(&reflist, self->child_i32_1);
+  push_objlist(&reflist, self->string);
   return reflist;
+}
+
+const char **get_string(GcPtr test_obj) {
+  const TestObj *test_obj_ = GC_GET(TestObj, test_obj);
+  return GC_GET(char *, test_obj_->string);
+}
+
+void destroy_string(void *p) {
+  char *s = *PTR_CAST(char **, p);
+  DBG_PRINTF("is called with on string %s\n", s);
+  xfree(s);
 }
 
 i32 main() {
   GcArena arena = gc_new_arena();
 
-  // Create number0 object.
-  i32 number0_ = 10;
-  GcPtr number0 = gc_new_object(&arena, PUT_ON_HEAP(number0_), new_objlist(), NO_DESTORY_CALLBACK);
+  // The simplist GC object (No custom destroyer, no child objects).
+  i32 number_ = 10;
+  GcPtr number = gc_new_object(&arena, PUT_ON_HEAP(number_), new_objlist(), NO_DESTORY_CALLBACK);
 
-  // Create number1 object.
-  i32 number1_ = 255;
-  GcPtr number1 = gc_new_object(&arena, PUT_ON_HEAP(number1_), new_objlist(), NO_DESTORY_CALLBACK);
+  // A GC Object with a custome destroyer.
+  const char s[] = "hello, world";
+  char *string_ = xalloc_(sizeof(s));
+  memcpy(string_, s, sizeof(s));
+  GcPtr string = gc_new_object(&arena, PUT_ON_HEAP(string_), new_objlist(), &destroy_string);
 
   // Create test_obj1 object.
   TestObj test_obj1_ = (TestObj){
-      .child_i32_0 = gc_clone(number0),
-      .child_i32_1 = gc_clone(number1),
+      .child_i32_0 = gc_clone(number),
+      .string = gc_clone(string),
   };
   GcPtr test_obj1 = gc_new_object(&arena, PUT_ON_HEAP(test_obj1_), test_obj_reflist(&test_obj1_), NO_DESTORY_CALLBACK);
 
   // Create test_obj2 object.
   TestObj test_obj2_ = (TestObj){
-      .child_i32_0 = number0,
-      .child_i32_1 = number1,
+      .child_i32_0 = number,
+      .string = string,
   };
   GcPtr test_obj2 = gc_new_object(&arena, PUT_ON_HEAP(test_obj2_), test_obj_reflist(&test_obj2_), NO_DESTORY_CALLBACK);
 
   // Print out addresses of GC pointers so later we can see which objects are destroyed.
-  DBG_PRINTF("number0   = "); println_gcptr_addr(number0);
-  DBG_PRINTF("number1   = "); println_gcptr_addr(number1);
-  DBG_PRINTF("test_obj1 = "); println_gcptr_addr(test_obj1);
-  DBG_PRINTF("test_obj2 = "); println_gcptr_addr(test_obj2);
+  DBG_PRINTF("number    = ");
+  println_gcptr_addr(number);
+  DBG_PRINTF("string    = ");
+  println_gcptr_addr(string);
+  DBG_PRINTF("test_obj1 = ");
+  println_gcptr_addr(test_obj1);
+  DBG_PRINTF("test_obj2 = ");
+  println_gcptr_addr(test_obj2);
 
   // Since we defined `DEBUG_LOG` ealier the `gc_sweep` functions would log which objects are destroyed.
   gc_sweep(&arena); // Expect: No objects are destroyed.
   gc_mark_dead(test_obj1);
-  gc_sweep(&arena); // Expect: `test_obj` is destroyed.
+  gc_sweep(&arena);                  // Expect: `test_obj1` is destroyed.
+  DBG_PRINT(*get_string(test_obj2)); // `test_obj2` is still alive and using `string`, so `string` is not destroyed.
   gc_mark_dead(test_obj2);
-  gc_sweep(&arena); // Expect: `test_obj2`, `number0`, `number1` are destroyed.
+  gc_sweep(&arena); // Expect: `test_obj2`, `number`, `string` are all destroyed.
   gc_sweep(&arena); // Expect: No Objects are destroyed.
 
   free_gcarena(arena);
